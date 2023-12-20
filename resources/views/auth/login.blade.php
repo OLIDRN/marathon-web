f2c31aa529bd05bffb7780a8da11174053d381ba
noah <101118381+nwyux@users.noreply.github.com>
2023-12-20 15:10:31 +0100

    register login css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,11 +2,21 @@
 
 @section('content')
 
-    <form action="{{route("login")}}" method="post">
-        @csrf
-        <input type="email" name="email" required placeholder="Email" /><br />
-        <input type="password" name="password" required placeholder="password" /><br />
-        Remember me<input type="checkbox" name="remember"   /><br />
-        <input type="submit" /><br />
-    </form>
+    <div class="flex flex-col justify-center gap-6 p-6 rounded-lg bg-stone-800 border-2 border-amber-400 shadow-2xl shadow-amber-700">
+        <h1 class="text-3xl font-bold text-center">Se connecter</h1>
+        <form action="{{route("login")}}" method="post" class="flex flex-col gap-4">
+            @csrf
+            <input type="email" name="email" required placeholder="Email" class="rounded p-1 text-noir focus:outline-none" />
+            <input type="password" name="password" required placeholder="Mot de passe" class="rounded p-1 text-noir focus:outline-none" />
+            <div class="flex jusitfy-center m-auto gap-2 items-center">
+            <p>Se rappeler de moi</p>
+            <input type="checkbox" name="remember" class="size-4 checked:accent-amber-400"/>
+            </div>
+            <input type="submit" class="transition-all border w-max m-auto px-2 py-1 border-amber-400 rounded hover:bg-amber-400" />
+        </form>
+        <div class="flex justify-center gap-2">
+            <p>Pas de compte ? <p>
+            <a class="underline" href="{{route("register")}}">Créez en un !</a>
+        </div>
+    </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,14 +1,20 @@
 @extends("templates.app")
 
 @section('content')
-<form action="{{route("register")}}" method="post">
-    @csrf
-    <input type="text" name="name" required placeholder="Name" /><br />
-    <input type="email" name="email" required placeholder="Email" /><br />
-    <input type="password" name="password" required placeholder="password" /><br />
-    <input type="password" name="password_confirmation" required placeholder="password" /><br />
-    <input type="submit" /><br />
-</form>
-Déjà un compte ? <a href="{{route("login")}}">Connectez vous</a>
 
+    <div class="flex flex-col justify-center gap-6 p-6 rounded-lg bg-stone-800 border-2 border-amber-400 shadow-2xl shadow-amber-700">
+        <h1 class="text-3xl font-bold text-center">Créer un compte</h1>
+        <form action="{{route("register")}}" method="post" class="flex flex-col gap-4">
+            @csrf
+            <input type="text" name="name" required placeholder="Nom d'utilisateur" class="rounded p-1 text-noir focus:outline-none" />
+            <input type="email" name="email" required placeholder="Email" class="rounded p-1 text-noir focus:outline-none" />
+            <input type="password" name="password" required placeholder="Mot de passe" class="rounded p-1 text-noir focus:outline-none" />
+            <input type="password" name="password_confirmation" required placeholder="Confirmation de mot de passe" class="rounded p-1 text-noir focus:outline-none" />
+            <input type="submit" class="transition-all border w-max m-auto px-2 py-1 border-amber-400 rounded hover:bg-amber-400"/>
+        </form>
+        <div class="flex justify-center gap-2">
+            <p>Déjà un compte ? <p>
+            <a class="underline" href="{{route("login")}}">Connectez vous</a>
+        </div>
+    </div>
 @endsection
