321c9d37d9e3614760720866aa29f8c266f129c5
Vallet Alexandre <alexandre_vallet@ens.univ-artois.fr>
2023-12-20 14:58:31 +0100

    ajout de la création d'histoire

--- resources/views/histoire/index.blade.php
+++ resources/views/histoire/index.blade.php
@@ -1,3 +1,6 @@
+<button class="btn btn-primary" onclick="window.location='{{ route('histoire.create') }}'">Ajouter une histoire</button>
+
+
 @foreach($histoire as $story)
     <h3>Titre : {{ $story->titre }}</h3>
     <h4>Resume : {{ $story->pitch }}</h4>
