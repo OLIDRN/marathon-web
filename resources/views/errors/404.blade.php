b4f173a9ff5aa8728c426defe4f8016d1afe3c72
noah <101118381+nwyux@users.noreply.github.com>
2023-12-21 16:06:41 +0100

    error 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,16 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>404</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends("templates.app")
 
-    @vite(["resources/css/normalize.css", 'resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-<main>
-    <div style="font-size: larger;">Error 404 - SVP, Stylisez moi !!</div>
-</main>
+@section('content')
 
-</body>
-</html>
+    <div class="error w-full h-screen"></div>
+
+    {{-- div error 404 --}}
+    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div class="flex flex-col justify-center gap-4 max-w-2xl m-auto">
+            <h1 class="text-3xl text-center">Oups</h1>
+            <p class="text-center text-xl">Vous vous êtes perdus ?</p>
+            <a class="m-auto text-lg before:content-['←'] underline" class="text-2xl underline" href="{{ route('index') }}"> Retourner en lieu sûr</a>
+        </div>
+
+
+
+@endsection
