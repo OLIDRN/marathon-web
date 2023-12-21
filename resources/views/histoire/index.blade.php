fa81a73be3e35b5758225f9c88c83f58fcf37a03
noah <101118381+nwyux@users.noreply.github.com>
2023-12-21 11:28:31 +0100

    lot of style

--- resources/views/histoire/index.blade.php
+++ resources/views/histoire/index.blade.php
@@ -8,16 +8,17 @@
         <form class="flex justify-center" method="POST" action="{{ route('histoire.un')  }}">
             @csrf
             @method('POST')
-            <select class="border border-gold p-1 bg-stone-800 focus:outline-none" id="cat" name="cat" onchange="this.form.submit()">
+            <select class="border border-gold rounded-lg p-1 bg-noir focus:outline-none" id="cat" name="cat" onchange="this.form.submit()">
                 <option value="" {{ $cat == null ? 'selected' : '' }}>Tout les genres</option>
                 @foreach($genres as $genre)
                     <option value="{{ $genre->id }}" {{ $cat == $genre->id ? 'selected' : '' }}>{{ $genre->label }}</option>
                 @endforeach
             </select>
         </form>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
         @foreach($histoire as $story)
             @if($story->active === 1)
-                <div class="flex flex-col justify-center max-w-md gap-4 p-6 rounded-lg border-2 border-gold shadow-2xl shadow-amber-700">
+                <div class="flex flex-col justify-center max-w-md gap-4 p-6 rounded-lg border-2 border-gold shadow-2xl shadow-amber-700 hover:scale-105 transition-all">
                     <h3 class="text-xl font-bold">{{ $story->titre }}</h3>
                     @if($story->id == 2)
                         <img class="aspect-square object-cover w-full" src="{{ $story->photo }}" alt="Image description">
@@ -43,8 +44,9 @@
                 </div>
             @endif
         @endforeach
+        </div>
     </div>
-    <button class="fixed bottom-0 right-0 m-4 p-2 bg-stone-700 rounded-full hover:bg-stone-600" onclick="window.location.href='{{ route('histoire.create') }}'">
+    <button class="fixed bottom-0 z-10 right-0 m-4 p-3 bg-noir rounded-xl hover:bg-gold border-2 border-gold" onclick="window.location.href='{{ route('histoire.create') }}'">
         Créer une histoire
     </button>
 @endsection
--- resources/views/histoire/starthistory.blade.php
+++ resources/views/histoire/starthistory.blade.php
@@ -1,10 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Start History</title>
-</head>
-<body>
-<h1>{{ $histoire->titre }}</h1>
-<p>{{ $histoire->pitch }}</p>
-</body>
-</html>
+@extends("templates.app")
+
+@section('content')
+
+
+    <div class="flex flex-col justify-center gap-10 p-6 max-w-2xl">
+    <h1>{{ $histoire->titre }}</h1>
+    <img src="{{ $histoire->photo }}" alt="Image description">
+    {{-- @if($histoire->id == 2)
+        <img src="{{ Vite::asset('/public' . $histoire->photo) }}" alt="image de l'histoire">
+    @else
+        <img src="{{ Storage::url($histoire->photo) }}" alt="image de lqdqd'histoire">
+    @endif     --}}
+    <p>{{ $histoire->pitch }}</p>
+
+    <div class="flex gap-2">
+    <a class="flex p-2 border border-gold w-max hover:bg-gold" href="#">Choix 1</a>
+    <a class="flex p-2 border border-gold w-max hover:bg-gold" href="#">Choix 2</a>
+    </div>
+    </div>
+
+@endsection
